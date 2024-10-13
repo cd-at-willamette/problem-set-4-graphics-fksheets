7b22cc49651e697ce05b1576be7271ac497cd8ed
fksheets <fksheets@willamette.edu>
2024-10-13 14:19:17 -0700

    Update Prob2.py

--- Prob2.py
+++ Prob2.py
@@ -1,25 +1,39 @@
 ########################################
-# Name:
+# Name: Flannery Sheets
 # Collaborators:
-# Estimated time spent (hrs):
+# Estimated time spent (hrs): 1
 ########################################
 
-from pgl import GWindow, GRect
+from pgl import *
 
-WIDTH = 800
+WIDTH = 600
 HEIGHT = 600
-BRICK_WIDTH = 40
-BRICK_HEIGHT = 20
-BRICKS_IN_BASE = 15
+BRICK_WIDTH = 70
+BRICK_HEIGHT = 70
+BRICKS_IN_BASE = 7
 
 def draw_pyramid():
-    """ 
-    Draws a pyramid of bricks centered on the screen with a height of BRICKS_IN_BASE. 
-    """
 
     gw = GWindow(WIDTH, HEIGHT)
 
-    # You got it from here
+    w = WIDTH
+    h = HEIGHT
+    x = BRICK_WIDTH
+    y = BRICK_HEIGHT
+    n = BRICKS_IN_BASE
+    
+    for i in range(n):
+        row = n - i
+        startx = (gw.get_width()- (row * x)) // 2
+        starty = gw.get_height() - (row +1) * y
+
+        for brick in range(row):
+            width = startx + brick * x
+            height = starty
+            brick_rect = GRect(width,height,x,y)
+            gw.add(brick_rect)
+
+
 
 
 
